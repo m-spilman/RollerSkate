a617f9c5c9a3fc2ca1198d35852ebbce7620f913
Matt Spilman <mspilman@Matts-MacBook-Pro.local>
2019-09-03 20:19:52 -0500

    just making sure we are up to date, and fixed background color issue, also this should match what is hosted

--- js/modals.js
+++ js/modals.js
@@ -1,51 +1,48 @@
 
-//sets the user status after a state change 
+// sets the user status after a state change
 let userStatus = false
-auth.onAuthStateChanged(user =>{
-    if (user) {userStatus = true}
+auth.onAuthStateChanged(user => {
+  if (user) { userStatus = true }
 })
-document.addEventListener('DOMContentLoaded', function(){
-    let modals = document.querySelectorAll('.modal')
-    M.Modal.init(modals)
-   
-    })
+document.addEventListener('DOMContentLoaded', function () {
+  let modals = document.querySelectorAll('.modal')
+  M.Modal.init(modals)
+})
 
 const signupForm = document.querySelector('#signup-form')
 signupForm.addEventListener('submit', (e) => {
-    e.preventDefault()
-    const email = signupForm['signup-email'].value
-    const password = signupForm['signup-password'].value
-    auth.createUserWithEmailAndPassword(email, password).then(cred => {
-        const modal = document.querySelector('#modal-signup')
-        M.Modal.getInstance(modal).close()
-        signupForm.reset()
-    })
+  e.preventDefault()
+  const email = signupForm['signup-email'].value
+  const password = signupForm['signup-password'].value
+  auth.createUserWithEmailAndPassword(email, password).then(cred => {
+    const modal = document.querySelector('#modal-signup')
+    M.Modal.getInstance(modal).close()
+    signupForm.reset()
+  })
 })
 
 const logout = document.querySelector('#logout')
 logout.addEventListener('click', e => {
-    e.preventDefault()
-    auth.signOut()
-    location.reload()
+  e.preventDefault()
+  auth.signOut()
+  location.reload()
 })
 
 const loginForm = document.querySelector('#login-form')
-loginForm.addEventListener('submit', (e) =>{
-    e.preventDefault()
-    const email = loginForm['login-email'].value
-    const password = loginForm['login-password'].value
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const email = loginForm['login-email'].value
+  const password = loginForm['login-password'].value
 
-    auth.signInWithEmailAndPassword(email, password).then(cred => {
-        const modal = document.querySelector('#modal-login')
-        M.Modal.getInstance(modal).close()
-        loginForm.reset()
-       
-
-    })
+  auth.signInWithEmailAndPassword(email, password).then(cred => {
+    const modal = document.querySelector('#modal-login')
+    M.Modal.getInstance(modal).close()
+    loginForm.reset()
+  })
 })
 
 const lyrics = document.querySelector('#lyrics-modal')
 logout.addEventListener('click', e => {
-    e.preventDefault()
-    M.Modal.getInstance(modal).close()
+  e.preventDefault()
+  M.Modal.getInstance(modal).close()
 })
--- js/userslist.js
+++ js/userslist.js
@@ -1,17 +1,14 @@
-var songsArray = [];
-document.addEventListener("DOMContentLoaded", function() {
-    auth.onAuthStateChanged(function(user) {
-    if(user)
-    {db.collection("songs").doc(auth.currentUser.uid).get().then(renderMe)} // if user is logged in, get their data and call renderMe
-    else
-    {renderNoUser()} // if user is not logged in call renderNoUser
+var songsArray = []
+document.addEventListener('DOMContentLoaded', function () {
+  auth.onAuthStateChanged(function (user) {
+    if (user) { db.collection('songs').doc(auth.currentUser.uid).get().then(renderMe) } // if user is logged in, get their data and call renderMe
+    else { renderNoUser() } // if user is not logged in call renderNoUser
+  })
 })
-
-})
-//renders the data retreived from the users document
-function renderMe(data) {
-  let songsArray = data.data().song;
-  songListHTML = songsArray.map(function(data) {
+// renders the data retreived from the users document
+function renderMe (data) {
+  let songsArray = data.data().song
+  let songListHTML = songsArray.map(function (data) {
     return `<div class = col-sm-3>
         <div class = movie card style = width: 18rem;>
         <img class = card-img-top src = ${data.cover} alt = 'Picture Unavailable'>
@@ -20,14 +17,13 @@ function renderMe(data) {
         <div class = card-title><h5> ${data.track} </h5></div>
         <div class = card-subtitle year><h5> ${data.artist} </h5></div>
         </div>
-        </div> </div> </div>`;
-  });
+        </div> </div> </div>`
+  })
 
-  document.getElementById("content").innerHTML =
-    "<div class = row>" + songListHTML.join("") + "</div>";
+  document.getElementById('content').innerHTML =
+    '<div class = row>' + songListHTML.join('') + '</div>'
 }
-function renderNoUser() // renders text letting the user know they are not logged in
+function renderNoUser () // renders text letting the user know they are not logged in
 {
-  document.getElementById("content").innerHTML = "<div class = 'col-12 header text-center' id='list-notSignedIn'><h1>Login to save content</h1></div>";
+  document.getElementById('content').innerHTML = "<div class = 'col-12 header text-center' id='list-notSignedIn'><h1>Login to save content</h1></div>"
 }
-
